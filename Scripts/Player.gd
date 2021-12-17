796f532d9a1e859a123c09d9aca904160c867043
Devpark <devpark06@gmail.com>
2021-12-17 15:38:23 +0530

    initial commit

---
+++ Scripts/Player.gd
@@ -0,0 +1,20 @@
+extends KinematicBody2D
+
+const Acceleration = 20
+const Max_speed = 100
+const Friction = 10
+var velocity = Vector2.ZERO
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity += input_vector * Acceleration * delta
+		velocity = velocity.clamped(Max_speed * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, Friction * delta)
+
+	move_and_collide(velocity)
---
+++ Scripts/Vectorcreator.gd
@@ -0,0 +1,37 @@
+extends Area2D
+
+signal vector_created(vector)
+export var maximum_lenght = 200
+var touch_down = false
+var position_start = Vector2.ZERO
+var position_end = Vector2.ZERO
+var vector = Vector2.ZERO
+	
+func _draw():
+	draw_line(position_start - global_position,(position_end - global_position),Color.blueviolet,8)
+	draw_line(position_start - global_position,(position_start - global_position + vector),Color.red,16)
+
+func reset():
+	position_start = Vector2.ZERO
+	position_end = Vector2.ZERO
+	vector = Vector2.ZERO
+	update()
+	
+func _input(event):		
+	if event.is_action_released("ui_touch"):
+		touch_down = false
+		emit_signal("vector_created", vector)
+		reset()
+	
+	if not touch_down:
+		return
+	
+	if event is InputEventMouseMotion:
+		position_end = event.position
+		vector = -(position_end - position_start).clamped(maximum_lenght)
+		update()
+
+func on_Vectorcreator_input_event(_viewport, event, _shape_idx):
+	if event.is_action_pressed("ui_touch"):
+		touch_down = true
+		position_start = event.position
